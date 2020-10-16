69601d16623f76ecec06d231371bb66993f922f9
saurya3d <saurabhchvn9200@gmail.com>
2020-10-16 11:56:32 +0530

    My first commit

---
+++ product/models.py
@@ -0,0 +1,50 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+
+
+# Create your models here.
+# from mptt.fields import TreeForeignKey
+
+
+class Category(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=500)
+    description = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/', null=False)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    amount = models.IntegerField(default=0)
+    minamount = models.IntegerField(default=3)
+    #variant = models.CharField(max_length=10, choices=VARIANTS, default='None')
+    detail = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
