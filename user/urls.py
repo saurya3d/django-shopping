b3e507b7bd9646424295db906c5b3d658edbae67
saurya3d <saurabhchvn9200@gmail.com>
2020-10-21 17:32:01 +0530

    Order completed

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,17 @@
 
 from django.urls import path, include
 
+
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('update', views.user_update, name='user_update'),
-    path('password', views.user_password, name='user_password')
+    path('password', views.user_password, name='user_password'),
+    path('orders/', views.user_orders, name='user_orders'),
+    path('orders_product/', views.user_order_product, name='user_order_product'),
+    path('orderdetail/<int:id>', views.user_orderdetail, name='user_orderdetail'),
+    path('orders_product_detail/<int:id>/<int:oid>', views.user_order_product_detail, name='user_order_product_detail'),
+
 ]
